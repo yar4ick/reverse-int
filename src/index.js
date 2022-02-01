b7e2324c562040076c8f4aa2d387a5fa424e4b59
yar4ick <y.hapalov@gmail.com>
2022-02-01 22:24:18 +0200

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let numStr = n.toString();
+    let finalStr = '';
+
+    for (let i = numStr.length - 1; i >= 0; i--)
+    {
+        finalStr += numStr[i];
+    }
+
+    return parseInt(finalStr);
 }
